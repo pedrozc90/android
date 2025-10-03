2287f6bb4ac2b53d95422ff508a67c47a571a64e
pedrozc90 <pedrozc90@gmail.com>
2025-10-03 14:02:40 -0300

    added apps/mars-photos

---
+++ apps/mars-photos/app/src/main/java/com/example/marsphotos/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.marsphotos
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.marsphotos.ui.MarsPhotosApp
+import com.example.marsphotos.ui.theme.MarsPhotosTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MarsPhotosTheme {
+                MarsPhotosApp()
+            }
+        }
+    }
+}
---
+++ apps/mars-photos/app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
@@ -0,0 +1,10 @@
+package com.example.marsphotos.network
+
+import retrofit2.http.GET
+
+interface MarsApiService {
+
+    @GET("photos")
+    suspend fun getPhotos(): List<MarsPhoto>
+
+}
